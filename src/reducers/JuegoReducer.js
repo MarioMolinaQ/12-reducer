fd4a953d93de52c1595840f2eaa1b5d2e3673539
mario <mario.molina.q@gmail.com>
2022-07-24 11:05:53 +0200

    Finalizado useReducer
    Finalizado useReducer

---
+++ src/reducers/JuegoReducer.js
@@ -0,0 +1,19 @@
+// Tiene 2 parametros, el estado por defecto en blanco y la acción a disparar o a lanzar.
+export const JuegoReducer = (state =[], action) => {
+   
+    switch (action.type) {
+        case "crear":
+            return [...state, action.payload]
+        
+        case "borrar":
+            return state.filter(juego => juego.id !== action.payload);
+        
+        case "editar":
+            let indice = state.findIndex(juego => juego.id === action.payload.id);
+            state[indice] = action.payload;
+            return [...state];
+            
+        default:
+            return state;
+    }
+}
